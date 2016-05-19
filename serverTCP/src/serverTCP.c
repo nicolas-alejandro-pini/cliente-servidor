daf9ca3eb19ee2670429f93b9dbf56715d5f8bf7
Nicolas A. Pini <nicolas.pini@gmail.com>
2016-05-19 20:57:21 -0300

    Agrego los dos proyectos, clientTCP y serverTCP

---
+++ serverTCP/src/serverTCP.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ServerTCP.c
+ Author      : Nicolas Pini
+ Version     :
+ Copyright   : http://www.gnu.org/licenses/
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("ServerTCP"); /* prints  */
+	return EXIT_SUCCESS;
+}
